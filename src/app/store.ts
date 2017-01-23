1e374b89639869dc4eb1e9484b3c3d1374e12aa6
Moshfegh Hamedani <moshfegh@live.com.au>
2017-01-23 18:51:09 +1100

    Refactor case statements.

--- src/app/store.ts
+++ src/app/store.ts
@@ -11,40 +11,51 @@ export const INITIAL_STATE: IAppState = {
   lastUpdate: null
 }
 
+function addTodo(state, action) {
+  var newTodo = { id: state.todos.length + 1, title: action.title };
+
+  return tassign(state, {
+    todos: state.todos.concat(newTodo),
+    lastUpdate: new Date()
+  });
+}
+
+function toggleTodo(state, action) {
+  var todo = state.todos.find(t => t.id === action.id);
+
+  // Now, we need to find the position of this item in the array. 
+  var index = state.todos.indexOf(todo);
+
+  return tassign(state, {
+    todos: [
+      ...state.todos.slice(0, index),
+      tassign(todo, { isCompleted: !todo.isCompleted }),
+      ...state.todos.slice(index + 1),
+    ],
+    lastUpdate: new Date()
+  });
+}
+
+function removeTodo(state, action) {
+  return tassign(state, {
+    todos: state.todos.filter(t => t.id !== action.id),
+    lastUpdate: new Date()
+  });
+}
+
+function clearTodos(state, action) {
+  return tassign(state, {
+    todos: [],
+    lastUpdate: new Date()
+  });
+}
+
 export function rootReducer(state: IAppState, action): IAppState {
   switch (action.type) {
-    case ADD_TODO: 
-      var newTodo = { id: state.todos.length + 1, title: action.title };
-
-      return tassign(state, {
-        todos: state.todos.concat(newTodo),
-        lastUpdate: new Date()
-      });
-
-    case TOGGLE_TODO: 
-      var todo = state.todos.find(t => t.id === action.id);
-
-      // Now, we need to find the position of this item in the array. 
-      var index = state.todos.indexOf(todo);
-
-      return tassign(state, {
-        todos: [
-          ...state.todos.slice(index + 1),
-        ],
-        lastUpdate: new Date()
-      });
-
-    case REMOVE_TODO: 
-      return tassign(state, {
-        todos: state.todos.filter(t => t.id !== action.id),
-        lastUpdate: new Date()
-      });
-
-    case CLEAR_TODOS: 
-     return tassign(state, {
-        todos: [],
-        lastUpdate: new Date()
-      });
+    case ADD_TODO: return addTodo(state, action);
+    case TOGGLE_TODO: return toggleTodo(state, action);
+    case REMOVE_TODO: return removeTodo(state, action);
+    case CLEAR_TODOS: return clearTodos(state, action);
   }
 
   return state; 
